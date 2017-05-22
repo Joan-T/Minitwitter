6455bf72456cf299024ab0714dea38cbfb8e7f58
Joan <jotoma90@gmail.com>
2017-05-22 19:18:39 +0200

    2.- Las query funcionan2.- Las query funcionan

--- bd/selects.sql
+++ bd/selects.sql
@@ -1,5 +1,15 @@
+SELECT * FROM usuario;
+
+SELECT * FROM mensajes;
+
 SELECT mensajes.texto
 	FROM usuario, mensajes
 	WHERE usuario.nombre = 'Jtomas'
-	AND usuario.nombre = mensajes.nombre;
-	
+	AND usuario.nombre = mensajes.nombre;
+	
+SELECT mensajes.texto
+	FROM usuario, mensajes
+	WHERE usuario.nombre = 'Ssoria'
+	AND usuario.nombre = mensajes.nombre;
+
+
