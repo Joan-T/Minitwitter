581f772f2e54599e213d90185f6b33da8ccb341a
Joan <jotoma90@gmail.com>
2017-05-29 19:22:24 +0200

    3.- comprobarPassword y guardarTwitt funcionan

--- bd/selects.sql
+++ bd/selects.sql
@@ -1,15 +1,15 @@
-SELECT * FROM usuario;
-
-SELECT * FROM mensajes;
-
+SELECT password FROM usuario WHERE nombre = 'Jtomas';
+
+SELECT * FROM mensajes;
+
 SELECT mensajes.texto
 	FROM usuario, mensajes
 	WHERE usuario.nombre = 'Jtomas'
-	AND usuario.nombre = mensajes.nombre;
-	
+	AND usuario.nombre = mensajes.nombre;
+	
 SELECT mensajes.texto
 	FROM usuario, mensajes
 	WHERE usuario.nombre = 'Ssoria'
-	AND usuario.nombre = mensajes.nombre;
+	AND usuario.nombre = mensajes.nombre;
+	
 
-
